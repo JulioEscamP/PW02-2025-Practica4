e61f66c6a678c511b2212fe144edcef80848cca1
JulioEscamP <00117220@uca.edu.sv>
2025-09-25 21:35:27 -0600

    Adding: First set of exercises

---
+++ 00117220_practica04_seccion2/EjercicioFinal/script.js
@@ -0,0 +1,12 @@
+var input = document.getElementById(textbox)
+
+function recordatorios(input) {
+    switch (key) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
